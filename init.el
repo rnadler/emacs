abaef9ccdbb6cbb74da4ee5d513c9ad6073b88f9
rnadler <robert.nadler@gmail.com>
2016-03-09 21:34:02 -0800

    Add YAML mode

--- init.el
+++ init.el
@@ -79,4 +79,8 @@
     (highlight-parentheses-mode t)))
 (global-highlight-parentheses-mode t)
 (setq dired-listing-switches "-lXGh --group-directories-first")
+;; YAML mode
+(add-to-list 'load-path "~/.emacs.d/elpa/yaml-mode-20160220.340")
+ (require 'yaml-mode)
+   (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
