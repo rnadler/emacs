7a5e41ac62687e4bfe8e05e6160c7417cd4fb96e
rnadler <robert.nadler@gmail.com>
2016-03-19 15:12:57 -0700

    Add use-package and helm

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil)
- )
+ '(magit-commit-arguments (quote ("--all"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,17 +16,22 @@
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
+;; use-package
+(setq use-package-always-ensure t)
+(setq use-package-verbose t)
+(use-package magit)
 ;; recent files
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 (add-hook 'find-file-hook (lambda () (linum-mode 1)))
 (global-set-key (kbd "C-x g") 'magit-status)
-(require 'neotree)
+;; Neotree
+(use-package neotree)
 (global-set-key [f8] 'neotree-toggle)
 (setq make-backup-files nil) ;; stop creating those backup~ files
 ;; tabbar
-(require 'tabbar)
+(use-package tabbar)
 (tabbar-mode)
 (setq tabbar-buffer-groups-function
        (lambda ()
@@ -43,7 +48,7 @@
 ;;(setq tabbar-ruler-popup-menu t)       ; get popup menu.
 ;;(setq tabbar-ruler-popup-toolbar t)    ; get popup toolbar
 ;;(setq tabbar-ruler-popup-scrollbar t)  ; show scroll-bar on mouse-move
-(require 'tabbar-ruler)
+(use-package tabbar-ruler)
 ;; IDO
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
@@ -57,7 +62,7 @@
 	    (width . 120)))
 (setq default-frame-alist (copy-alist initial-frame-alist))
 ;; Parentheses highlight
-(require 'highlight-parentheses)
+(use-package highlight-parentheses)
 (define-globalized-minor-mode global-highlight-parentheses-mode
   highlight-parentheses-mode
   (lambda ()
@@ -65,21 +70,27 @@
 (global-highlight-parentheses-mode t)
 (setq dired-listing-switches "-lXGha --group-directories-first")
 ;; YAML mode
- (require 'yaml-mode)
-   (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(use-package yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 ;; Omit mode
 (require 'dired-x)
-    (setq-default dired-omit-files-p t) ; Buffer-local variable
+(setq-default dired-omit-files-p t) ; Buffer-local variable
 (setq dired-omit-files (concat dired-omit-files "\\|^\\..+$"))
 ;; Dired+
-(setq diredp-hide-details-initially-flag nil)
-(require 'dired+)
+(use-package dired+
+  :init
+  (setq diredp-hide-details-initially-flag nil))
 ;; Typescript mode
-(require 'typescript-mode)
+(use-package typescript-mode)
 ;; Markdown mode
  (autoload 'markdown-mode "markdown-mode"
        "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+;; Helm
+(use-package helm)
+(require 'helm-config)
+(helm-mode 1)
+(global-set-key (kbd "M-x") 'helm-M-x)
 
