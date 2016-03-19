2f6c8beeb818b02886f93a6a0b89f5fd7a135020
bobn <bob.nadler@resmed.com>
2016-03-18 17:12:59 -0700

    Add markdown mode

--- init.el
+++ init.el
@@ -76,4 +76,10 @@
 (require 'dired+)
 ;; Typescript mode
 (require 'typescript-mode)
+;; Markdown mode
+ (autoload 'markdown-mode "markdown-mode"
+       "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
