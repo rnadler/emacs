754734aacd7a27cf3e2930e581d95ab4b2218479
Bob Nadler <bobnadler@resmed.com>
2018-11-26 20:16:59 -0800

    Add sly and racket-mode

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
  '(magit-commit-arguments (quote ("--all")))
  '(package-selected-packages
    (quote
-    (company ivy-posframe beacon counsel-gtags flyspell-correct-ivy ivy-rich ivy-hydra smex flx counsel powerline-evil org-bullets htmlize multiple-cursors which-key php-mode yaml-mode use-package typescript-mode tabbar-ruler popup neotree markdown-mode magit jtags highlight-parentheses feature-mode dired-sort-menu dired-sort dired+ csv-mode csharp-mode php-mode)))
+    (racket-mode sly company ivy-posframe beacon counsel-gtags flyspell-correct-ivy ivy-rich ivy-hydra smex flx counsel powerline-evil org-bullets htmlize multiple-cursors which-key php-mode yaml-mode use-package typescript-mode tabbar-ruler popup neotree markdown-mode magit jtags highlight-parentheses feature-mode dired-sort-menu dired-sort dired+ csv-mode csharp-mode php-mode)))
  '(recentf-max-saved-items 30)
  '(ediff-split-window-function (quote split-window-horizontally))
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
@@ -192,3 +192,13 @@
 ;; Company mode
 (use-package company)
 (add-hook 'after-init-hook 'global-company-mode)
+;; Sly
+(use-package sly
+  :defer t
+  :config
+  (setq inferior-lisp-program "/usr/local/bin/sbcl"))
+;; Racket mode
+(use-package racket-mode
+  :defer t
+  :config
+  (setq racket-program "/home/bobn/racket/bin/racket"))
