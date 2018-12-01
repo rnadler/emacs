2ebb28c37b1471b461a2420a4888f665f8dd48a6
rnadler <robert.nadler@gmail.com>
2018-12-01 12:37:19 -0800

    Add clojure-mode and cider

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
  '(magit-commit-arguments (quote ("--all")))
  '(package-selected-packages
    (quote
-    (racket-mode sly company ivy-posframe beacon counsel-gtags flyspell-correct-ivy ivy-rich ivy-hydra smex flx counsel powerline-evil org-bullets htmlize multiple-cursors which-key php-mode yaml-mode use-package typescript-mode tabbar-ruler popup neotree markdown-mode magit jtags highlight-parentheses feature-mode dired-sort-menu dired-sort dired+ csv-mode csharp-mode php-mode)))
+    (cider clojure-mode racket-mode sly company ivy-posframe beacon counsel-gtags flyspell-correct-ivy ivy-rich ivy-hydra smex flx counsel powerline-evil org-bullets htmlize multiple-cursors which-key php-mode yaml-mode use-package typescript-mode tabbar-ruler popup neotree markdown-mode magit jtags highlight-parentheses feature-mode dired-sort-menu dired-sort dired+ csv-mode csharp-mode php-mode)))
  '(recentf-max-saved-items 30)
  '(ediff-split-window-function (quote split-window-horizontally))
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
@@ -202,3 +202,9 @@
   :defer t
   :config
   (setq racket-program "/home/bobn/racket/bin/racket"))
+(use-package clojure-mode
+  :defer t)
+(use-package cider
+  :defer t
+  :config
+  (setq cider-repl-display-help-banner nil))
