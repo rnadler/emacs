7cc4530d9e4ba48c1220188ce9d94ca77d53b016
rnadler <robert.nadler@gmail.com>
2020-03-29 17:05:50 -0700

    Add Doom config

---
+++ doom/config.el
@@ -0,0 +1,67 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Bob Nadler"
+      user-mail-address "robert.nadler@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "monospace" :size 14))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;;(display-time-mode t)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x l") 'display-line-numbers-mode)
+(global-set-key (kbd "C-x t") 'toggle-truncate-lines)
+(global-set-key [f8] 'treemacs)
+(setq make-backup-files nil) ;; stop creating those backup~ files
+
+(setq-default dired-omit-files-p t) ; Buffer-local variable
+(defun my/disable-line-numbers (&optional _)
+  (display-line-numbers-mode -1))
+
+(load! "./org-config.el")
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
---
+++ doom/org-config.el
@@ -0,0 +1,142 @@
+;; Org mode
+(defconst my-org-directory
+  (if (string= (system-name) "WIN10R90H8MKJ")
+      "//brutus/it/healthinformatics/BobN/org"
+      "~/Dropbox/org"))
+(defconst todo-org-file (concat my-org-directory "/todo.org"))
+(defconst journal-org-file (concat my-org-directory "/journal.org.gpg"))
+(defconst transfer-org-file "/media/sf_healthinformatics/BobN/org/transfer.org")
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(global-set-key (kbd "C-c o") 
+                (lambda () (interactive) (find-file todo-org-file)))
+(setq org-log-done t)
+(setq org-hide-leading-stars nil)
+(setq org-startup-indented t)
+(unless (eq system-type 'windows-nt)
+  (add-hook 'org-mode-hook 'turn-on-flyspell 'append))
+(setq org-todo-keywords
+      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)"))))
+(setq org-todo-keyword-faces
+      (quote (("TODO" :foreground "red" :weight bold)
+              ("NEXT" :foreground "blue" :weight bold)
+              ("DONE" :foreground "forest green" :weight bold)
+              ("WAITING" :foreground "orange" :weight bold)
+              ("HOLD" :foreground "magenta" :weight bold)
+              ("CANCELLED" :foreground "forest green" :weight bold))))
+(define-key global-map "\C-cc" 'org-capture)
+
+(cl-defun my/make/org-capture-template
+   (shortcut heading &optional (no-todo nil) (description heading) (category heading) (scheduled t))
+  "Quickly produce an org-capture-template."
+  `(,shortcut ,description entry
+	      (file+headline org-default-notes-file
+			     ,(concat heading "\n#+CATEGORY: " category))
+	      ,(concat "*" (unless no-todo " TODO") " %?\n"
+			(when nil ;; this turned out to be a teribble idea.
+			  ":PROPERTIES:\n:"
+			  (if scheduled
+			      "SCHEDULED: %^{Any time ≈ no time! Please schedule this task!}t"
+			    "CREATED: %U")
+			  "\n:END:") "\n\n ")
+		:empty-lines 1 :time-prompt t))
+
+;; (setq org-capture-templates
+;;       (loop for (shortcut heading)
+;;             in (seq-partition '("t" "Tasks, Getting Things Done"
+;;                                "r" "Research"
+;;                                "2" "2FA3"
+;;                                "m" "Email"
+;;                                "e" "Emacs (•̀ᴗ•́)و"
+;;                                "b" "Blog"
+;;                                "a" "Arbitrary Reading and Learning"
+;;                                "p" "Personal Matters") 2)
+;;             collect (my/make/org-capture-template shortcut heading)))
+
+(setq org-capture-templates
+      (cons (my/make/org-capture-template "b" "Blog")
+	    '(("t" "Todo" entry (file+headline todo-org-file "Tasks")
+	       "* TODO %?\n  %i\n  %a\n%T")
+	      ("x" "Transfer" entry (file+headline transfer-org-file "Tasks")
+	       "* TODO %?\n  %i\n  %a\n%T")
+	      ("j" "Journal" entry (file+olp+datetree journal-org-file)
+	       "* %?\nEntered on %U\n  %i\n  %a"))))
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((shell . t)
+   (ruby . t)
+   (emacs-lisp . t)))
+(setq org-agenda-include-diary t)
+(setq org-confirm-babel-evaluate nil)
+(fset 'do-org-backup
+   [?\C-u ?\C-x ?s ?\C-c ?o ?\M-< ?\C-r ?\C-s ?: ?  ?b ?a ?c ?k ?u ?p return ?\C-n ?\C-n ?\C-c ?\C-c])
+(global-set-key (kbd "C-c b")
+   (lambda ()
+     (interactive)
+     (let (
+         (oldp (point))
+         (oldbuff (current-buffer)))
+       (execute-kbd-macro (symbol-function 'do-org-backup))
+       (unless (eq (current-buffer) oldbuff) (switch-to-buffer oldbuff))
+       (goto-char oldp))
+     (message "Backup complete.")))
+(fset 'my-agenda
+   (lambda (&optional arg) "Startup my custom agenda." (interactive "p") (kmacro-exec-ring-item (quote ("ap" 0 "%d")) arg)))
+(use-package htmlize
+    :defer t)
+;; Custom agenda
+(setq org-agenda-dim-blocked-tasks nil)
+(setq org-agenda-compact-blocks t)
+(setq org-agenda-window-setup 'current-window)
+(setq org-agenda-files (list
+			todo-org-file
+			"~/Projects/emacs/shared.org"
+			transfer-org-file))
+(when (string= (system-name) "bob-x1-carbon")
+    (setq org-agenda-files (delete transfer-org-file org-agenda-files)))
+(setq org-refile-targets '((org-agenda-files :maxlevel . 2)))
+(setq org-refile-use-outline-path 'file)
+(setq org-refile-allow-creating-parent-nodes 'confirm)
+(setq org-agenda-tags-todo-honor-ignore-options t)
+(setq org-agenda-custom-commands
+      '(("p" "Agenda and Projects"
+         ((agenda "" nil)
+	  (tags-todo "-TODO=\"DONE\"-TODO=\"CANCELLED\""
+		((org-agenda-skip-function '(org-agenda-skip-entry-if 'timestamp))
+		 (org-agenda-overriding-header "Unscheduled TODO entries:")
+		 (org-agenda-view-columns-initially t)
+		 (org-agenda-sorting-strategy '(tag-up alpha-up))))
+          (tags "project"
+              ((org-agenda-overriding-header "Projects:")
+               (org-tags-match-list-sublevels 'indented)))))
+	))
+;; Nice looking bullets for headings
+(use-package org-bullets
+  :ensure t
+  :init
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+(setq org-ellipsis "⤵")
+(add-hook 'org-mode-hook 'my/disable-line-numbers)
+(add-hook 'org-agenda-finalize-hook 'my/disable-line-numbers)
+;; Ivy fix
+(setq org-refile-use-outline-path 'file
+      org-outline-path-complete-in-steps nil)
+;; Other stuff
+(setq org-catch-invisible-edits 'show-and-error)
+(setq org-list-demote-modify-bullet '(("+" . "-") ("-" . "+") ("*" . "+")))
+(add-hook 'org-mode-hook (lambda ()
+   "Beautify Org Checkbox Symbol"
+   (push '("[ ]" . "☐") prettify-symbols-alist)
+   (push '("[X]" . "☑" ) prettify-symbols-alist)
+   (push '("[-]" . "❍" ) prettify-symbols-alist)
+   (prettify-symbols-mode)))
+(defface org-checkbox-done-text
+  '((t (:foreground "#71696A")))
+  "Face for the text part of a checked org-mode checkbox.")
+(font-lock-add-keywords
+ 'org-mode
+ `(("^[ \t]*\\(?:[-+*]\\|[0-9]+[).]\\)[ \t]+\\(\\(?:\\[@\\(?:start:\\)?[0-9]+\\][ \t]*\\)?\\[\\(?:X\\|\\([0-9]+\\)/\\2\\)\\][^\n]*\n\\)"
+    1 'org-checkbox-done-text prepend))
+ 'append)
