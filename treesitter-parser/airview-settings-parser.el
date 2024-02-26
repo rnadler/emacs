991e27358bc52cff3f79899f0aa3d61ee4dcac86
rnadler <robert.nadler@gmail.com>
2024-02-25 17:09:08 -0800

    Update treesitter-parser.

--- treesitter-parser/airview-settings-parser.el
+++ treesitter-parser/airview-settings-parser.el
@@ -1,5 +1,5 @@
 
-;; Use treesitter to parse Java source code and extract field names
+;; Use treesitter to parse Java source code and extract field names and properties
 ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Pattern-Matching.html
 
 (require 'cl-lib)
@@ -8,8 +8,7 @@
       '((variable_declarator (identifier) @field-name)))
 
 (setq mod-query
-      '((modifiers (annotation) @ann-name
-         (marker_annotation) @m-ann-name)))
+      '((modifiers (annotation) @ann-name)))
 
 (defun java-field-name (nodes)
   (cl-dolist (node nodes)
@@ -18,7 +17,9 @@
         (let ((n (cdr (assoc 'field-name qres))))
           (progn
             (message "field: %s mods=%s" (treesit-node-text n)
-                     (prin1-to-string (treesit-query-capture (treesit-node-parent node) mod-query)))
+                     (mapconcat (lambda (mod) (treesit-node-text (cdr mod)))
+                                (treesit-query-capture (treesit-node-parent node) mod-query) ", ")
+                     )
             ;; (message "field: %s sibs=%s" (treesit-node-text n)
             ;;          (prin1-to-string (treesit-node-children (treesit-node-parent node))))
             (cl-return))
@@ -26,14 +27,15 @@
     )
   )
 
-(let* ((parser (treesit-parser-create 'java (get-buffer "FlowGenSettings.java")))
+(defun parse-class (class-nodes)
+  (message "class child count = %d" (treesit-node-child-count class-nodes))
+  (cl-dolist (child (treesit-node-children class-nodes))
+    (java-field-name (treesit-node-children child))))
+
+(let* ((parser (treesit-parser-create 'java (get-buffer
+                                             "FlowGenSummaryDataProperties.java"  ;; "FlowGenSettings.java"
+                                             )))
        (root-node (treesit-parser-root-node parser))
-       ;; Field name searching does not work...
-       (class-body (treesit-node-child (treesit-node-child root-node -1) -1)))
-  ;;(message (buffer-file-name (treesit-parser-buffer parser)))
-  ;;(treesit-node-child-by-field-name root-node "class_declaration")
-  (message "class child count = %d" (treesit-node-child-count class-body))
-  (cl-dolist (child (treesit-node-children class-body))
-    (java-field-name (treesit-node-children child))
-    )
+       (class-nodes (treesit-search-subtree root-node "class_body")))
+  (parse-class class-nodes)
   )
