571e4554b181b87592334f2505fc3351281e4bf2
rnadler <robert.nadler@gmail.com>
2024-03-03 17:26:39 -0800

    More treesitter-parser updates.

--- treesitter-parser/airview-settings-parser.el
+++ treesitter-parser/airview-settings-parser.el
@@ -7,18 +7,29 @@
 (setq field-query
       '((variable_declarator (identifier) @field-name)))
 
+(setq type-query
+      '((field_declaration (type_identifier) @type-name)))
+
 (setq mod-query
       '((modifiers (annotation) @ann-name)))
+
+(defun query-val (qres key)
+  (cdr (assoc key qres)))
 
 (defun java-field-name (nodes)
   (cl-dolist (node nodes)
     (let ((qres (treesit-query-capture node field-query)))
       (when (not (null qres))
-        (let ((n (cdr (assoc 'field-name qres))))
+        (let* ((n (query-val qres 'field-name))
+               (parent (treesit-node-parent node))
+               (type (query-val (treesit-query-capture parent type-query) 'type-name))
+               (mods (treesit-query-capture parent mod-query)))
           (progn
-            (message "field: %s mods=%s" (treesit-node-text n)
-                     (mapconcat (lambda (mod) (treesit-node-text (cdr mod)))
-                                (treesit-query-capture (treesit-node-parent node) mod-query) ", "))
+            (message "field: %s type=%s mods(%d)=%s"
+                     (treesit-node-text n)
+                     (treesit-node-text type)
+                     (length mods)
+                     (mapconcat (lambda (mod) (treesit-node-text (cdr mod))) mods ", "))
             (cl-return))
           )))))
 
@@ -36,12 +47,24 @@
 ;; ------------------------------------------------------------
 ;; Execute the parser
 
-(setq java-file
-      "FlowGenSettings.java"
-      ;; "FlowGenSummaryDataProperties.java"
-      ;; "FlowGenClimateSummaryDataProperties.java"
-      ;; "FlowGenVentilatorSettings.java"
-      )
+(setq java-classes-dir "./flowGenClasses/")
+
+(setq java-files
+      '(
+       ("FlowGenSettings.java" . 'flow_gen_settings)
+       ("FlowGenVentilatorSettings.java" . 'flow_gen_ventilatory_settings)
+       ("FlowGenSummaryDataProperties.java" . 'flow_gen_summary_data)
+       ("FlowGenClimateSummaryDataProperties.java" . 'flow_gen_summary_data)
+      ))
+
+(defun open-file-buffer (fpath file-name)
+  (when (null (get-buffer file-name))
+    (find-file-noselect (concat fpath file-name))))
+
+(dolist (f java-files) (open-file-buffer java-classes-dir (car f)))
+
+(setq settings-java-file (car (car java-files)))
+(setq java-file (car (nth 2 java-files))) ;; summary data
 
 (let ((buffer (get-buffer java-file)))
   (if (null buffer)
